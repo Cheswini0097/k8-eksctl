70b690e5bf2927cb5a309eeeae379c36c8aa1905
Chethan <cheswinkumar@gmail.com>
2025-05-01 16:33:02 +0530

    first commit

---
+++ variables.tf
@@ -0,0 +1,18 @@
+variable "sg_id" {
+  type        = string
+  default     = "sg-01203de54c6f0eb13"
+  description = "description"
+}
+
+variable "public_subnet_id" {
+    type = string
+    default = "subnet-0a1c1237ef6834598"
+}
+variable "project_name" {
+  default = "coding_chalange"
+  
+}
+variable "environment" {
+  default = "chalanage"
+  
+}
